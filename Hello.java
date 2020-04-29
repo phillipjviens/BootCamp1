539203ace7ddab703e4442061c3e9234cc6176d7
Phillip Viens <38144672+phillipjviens@users.noreply.github.com>
2020-04-29 12:05:27 -0700

    Created Hello.java
    Simple program to understand basics of Java syntax

---
+++ Hello.java
@@ -0,0 +1,12 @@
+//Phillip Vines
+//Hello.java
+//Purpose: This is a sample program that will print
+//       "Hello, World!" to screen.
+
+public class Hello {
+        public static void main(String[] args){
+                System.out.println("Hello, World!");
+        }
+
+
+}
