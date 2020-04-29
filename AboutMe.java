b6f6ebb2ff2d07e284e8e31d768fab8ed855ede4
Phillip Viens <38144672+phillipjviens@users.noreply.github.com>
2020-04-29 12:11:20 -0700

    Create AboutMe.java
    A Simple program to familiarize myself with printing text in Java

---
+++ AboutMe.java
@@ -0,0 +1,16 @@
+//Phillip Viens
+//AboutMe.java
+//Simple program that displays:
+//Your Name
+//Your favorite Color
+//Your Place of Birth
+//
+
+public class AboutMe{
+        public static void main(String[] args){
+                System.out.println("Name: Phillip Viens");
+                System.out.println("Favorite Color: Blue");
+                System.out.println("Place of Birth: Montreal");
+        }
+
+}
